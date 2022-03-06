5dd42089eacb486cdc8327542d9094ca7de278ea
Tafimups <78031351+Tafimups@users.noreply.github.com>
2022-03-06 20:47:51 +0100

    Add files via upload

---
+++ oper.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+
+{
+    double A = sqrt(25);
+    double B = pow(2,5);
+
+    float D = log(3);
+    float C = tan(45);
+
+
+
+    printf("%lf\n", A);
+    printf("%lf\n", B);
+    printf("%lf\n", C);
+    printf("%lf\n", D);
+    return 0;
+}
