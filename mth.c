5f0c674827b8454d5e9907f8ff7877dc972e2587
Tafimups <78031351+Tafimups@users.noreply.github.com>
2022-03-06 20:47:02 +0100

    Add files via upload

---
+++ mth.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+
+{
+    double A = sqrt(25);
+    double B = pow(2,5);
+
+    float D = log(3);
+
+    printf("%lf\n", A);
+    printf("%lf\n", B);
+    
+    printf("%lf\n", A);
+    return 0;
+}
